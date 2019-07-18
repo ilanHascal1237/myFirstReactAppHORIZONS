d14298c3f5721012205558652ba145c7c1305cd7
Ilan Hascal <ihascal@brandeis.edu>
2019-07-18 16:02:27 -0400

    Was able to make all the compenents & a test list with dummy data

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>Ilan Hascal</p>, document.getElementById("root"));
+const dummyData = ["go home", "laundry", "pick up supplies"];
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <li>
+        <button>X</button> {this.props.text}
+      </li>
+    );
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="TODO task" />
+        <input type="submit" placeholder="Submit your task" />
+      </div>
+    );
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.data.map(data => (
+          <Todo text={data} />
+        ))}
+      </ul>
+    );
+  }
+}
+ReactDOM.render(<TodoList data={dummyData} />, document.getElementById("root"));
