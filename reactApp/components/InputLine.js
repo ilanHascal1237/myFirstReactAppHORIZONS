c5abc989f3075c95fa5b21141d958ced7d160718
Ilan Hascal <ihascal@brandeis.edu>
2019-07-19 14:06:27 -0400

    Added handleSubmit() & handleTyping()..now able to add TODO with custom text

--- reactApp/components/InputLine.js
+++ reactApp/components/InputLine.js
@@ -3,16 +3,31 @@ import React from "react";
 class InputLine extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      typedText: ""
+    };
   }
+  handleTyping(event) {
+    //////
+    event.preventDefault();
+    this.setState({ typedText: event.target.value });
+  }
+
+  handleSubmit() {
+    this.props.submit(this.state.typedText);
+    this.setState({ typedText: "" });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="TODO task" />
         <input
-          type="submit"
-          onClick={() => this.props.submit("Test Task")}
-          placeholder="Submit your task"
+          type="text"
+          placeholder="TODO task"
+          value={this.state.typedText}
+          onChange={event => this.handleTyping(event)}
         />
+        <input type="submit" onClick={() => this.handleSubmit()} placeholder="Submit your task" />
       </div>
     );
   }
